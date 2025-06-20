45bb0f8260844ef3754c1308a6dd56a7cc34dde4
DEV-DIEGOC-107\Diego <diego.visualsoftware@gmail.com>
2025-06-20 14:48:53 -0300

    same fix done before, but in method printImageInBytes

--- android/src/main/java/id/kakzaki/blue_thermal_printer/BlueThermalPrinterPlugin.java
+++ android/src/main/java/id/kakzaki/blue_thermal_printer/BlueThermalPrinterPlugin.java
@@ -888,6 +888,7 @@ public class BlueThermalPrinterPlugin implements FlutterPlugin, ActivityAware,Me
       Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
       if (bmp != null) {
         byte[] command = Utils.decodeBitmap(bmp);
+        THREAD.write(PrinterCommands.ESC);
         THREAD.write(PrinterCommands.ESC_ALIGN_CENTER);
         THREAD.write(command);
       } else {
